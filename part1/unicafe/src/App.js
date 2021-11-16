a30f9905a4ef9d822ebdcfaaa24b191e1db8327d
Matilda Mared <matildamared@TildasMacBookPro.local>
2021-11-16 08:07:01 +0100

    complete unicafe exercise step 2

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -4,6 +4,19 @@ const Button = ({ onClick, text }) => {
 	return <button onClick={onClick}>{text}</button>;
 };
 
+const Average = ({ good, neutral, bad }) => {
+	const total = good + neutral + bad;
+	let scores = good * 1 - bad * 1;
+
+	return <p>average {scores / total || "-"}</p>;
+};
+
+const Positive = ({ good, neutral, bad }) => {
+	const total = good + neutral + bad;
+
+	return <p>positive {(good / total) * 100 || "-"} %</p>;
+};
+
 const App = () => {
 	const [good, setGood] = useState(0);
 	const [neutral, setNeutral] = useState(0);
@@ -34,6 +47,9 @@ const App = () => {
 				<p>good {good}</p>
 				<p>neutral {neutral}</p>
 				<p>bad {bad}</p>
+				<p>all {good + neutral + bad}</p>
+				<Average good={good} neutral={neutral} bad={bad} />
+				<Positive good={good} neutral={neutral} bad={bad} />
 			</section>
 		</div>
 	);
